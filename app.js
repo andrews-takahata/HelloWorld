f877b12a96dac443db03477e70bec89bfc4c3cbd
andrews-takahata <andrews.takahata@interadapt.com.br>
2019-03-18 22:13:19 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -13,4 +13,38 @@ app.get('/api/today', function (req, res) {
     res.send(date);
 });
 
+
+
+app.get('/api/consulta-userr', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'root',
+        password: 'senhaFiap',
+        server: 'mysql', 
+        database: 'castillo' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from user', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
 module.exports = app
